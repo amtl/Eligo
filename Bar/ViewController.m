a4a41a7de5574527a1ab10ee03e67140f762a313
Andrea Mittelstaedt <amtl@fb.com>
2015-04-18 18:49:38 -0700

    Initial Commit

---
+++ Bar/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Bar
+//
+//  Created by Andrea Mittelstaedt on 18/04/15.
+//  Copyright (c) 2015 Andrea. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
